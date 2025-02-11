c9e53c5970df06a63564d072afdea0f9c63e5fbf
Grace Couch <gracetcouch@gmail.com>
2025-02-11 16:30:12 +0000

    Dummy UnitTest

--- ChineseNumberGameTests/ChineseNumberGameTests.swift
+++ ChineseNumberGameTests/ChineseNumberGameTests.swift
@@ -10,8 +10,26 @@ import Testing
 
 struct ChineseNumberGameTests {
 
-    @Test func example() async throws {
-        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    @Test func myMaxTest() async throws {
+       // 1) Set up data
+        let dataArray = [
+            (x: 3, y: 10, z: 7),
+            (x: 3, y: 19, z: 75),
+            (x: 6, y: 11, z: 7),
+            (x: 3, y: 6, z: 9),
+            (x: 3, y: 4, z: 7),
+            (x: 15, y: 10, z: 7)
+        ]
+        // 2) Expected result
+        let expectedResults: [Int] = [10, 75, 11, 9, 7, 15]
+
+        // 3) Test
+        for i in 0..<dataArray.count {
+            let data = dataArray[i]
+            let expectedResult = expectedResults[i]
+            let evaluatedResult = myMax(x: data.x, y: data.y, z: data.z)
+            #expect(expectedResult == evaluatedResult)
+        }
     }
 
 }
