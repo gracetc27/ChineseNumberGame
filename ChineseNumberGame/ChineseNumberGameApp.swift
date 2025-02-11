9f4b544c42e47837241ab790826dd618ec7ce2b3
Grace Couch <gracetcouch@gmail.com>
2025-02-11 15:47:34 +0000

    App setup

---
+++ ChineseNumberGame/ChineseNumberGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChineseNumberGameApp.swift
+//  ChineseNumberGame
+//
+//  Created by Grace couch on 11/02/2025.
+//
+
+import SwiftUI
+
+@main
+struct ChineseNumberGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ChineseNumberGameTests/ChineseNumberGameTests.swift
@@ -0,0 +1,17 @@
+//
+//  ChineseNumberGameTests.swift
+//  ChineseNumberGameTests
+//
+//  Created by Grace couch on 11/02/2025.
+//
+
+import Testing
+@testable import ChineseNumberGame
+
+struct ChineseNumberGameTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
