f78c299ddfb844ffea239f9d61f24ef97344ad80
Grace Couch <gracetcouch@gmail.com>
2025-02-12 17:21:19 +0000

    tdd chinese test

---
+++ ChineseNumberGame/Chinese Support/Chinese.swift
@@ -0,0 +1,25 @@
+//
+//  Chinese.swift
+//  ChineseNumberGame
+//
+//  Created by Grace couch on 12/02/2025.
+//
+import Foundation
+
+
+struct Chinese {
+    static func num99toChinese(num: Int) -> (chinese: String, pinyin: String)? {
+        let nums: [(String, String)] = [("零","líng"),
+        ("一", "yī"),
+        ("二", "èr"),
+        ("三", "sān"),
+        ("四", "sì"),
+        ("五", "wǔ"),
+        ("六", "liù"),
+        ("七", "qī"),
+        ("八", "bā"),
+        ("九", "jiǔ"),
+        ("十", "shí")]
+        return nil
+    }
+}
--- ChineseNumberGameTests/ChineseNumberGameTests.swift
+++ ChineseNumberGameTests/ChineseNumberGameTests.swift
@@ -32,4 +32,34 @@ struct ChineseNumberGameTests {
         }
     }
 
+    @Test func chineseTest() async throws {
+        let nums = [0, 8, 17, 20, 33, 40, 54, 63, 71, 87, 99]
+        let nilNums = [-10, 100, -1, 110, 104]
+
+        let expectedResults: [(chinese: String, pinyin: String)?] = [
+            ("零","líng"),
+            ("八", "bā"),
+            ("十七", "shí qī"),
+            ("二十", "èr shí"),
+            ("三十三", "sān shí sān"),
+            ("四十", "sì shí"),
+            ("五十四", "wǔ shí sì"),
+            ("六十三", "liù shí sān"),
+            ("七十一", "qī shí yī"),
+            ("八十七", "bā shí qī"),
+            ("九十九", "jiǔ shí jiǔ")
+        ]
+
+        for i in 0..<nums.count {
+            let number1 = Chinese.num99toChinese(num: nums[i])
+            let number2 = expectedResults[i]
+            if let number1, let number2 {
+                #expect(number1.chinese == number2.chinese)
+            }
+        }
+        for num in nilNums {
+            #expect(Chinese.num99toChinese(num: num) == nil)
+        }
+    }
+
 }
